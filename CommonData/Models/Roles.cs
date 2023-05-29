d4c563d697a2a2309dc1d9dd04e56e269d0ae6df
Bollumanikishore <96240131+Bollumanikishore@users.noreply.github.com>
2023-05-29 23:29:40 +0530

    added docs of inventory management

---
+++ CommonData/Models/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonData
+{
+    public class Roles
+    {
+        public string? RoleName { get; set; }
+        public string? Modules { get; set; }
+        public bool? Status { get; set; }
+    }
+}
